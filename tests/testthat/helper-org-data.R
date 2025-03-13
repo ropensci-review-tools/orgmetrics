2bf00d028c1c5a03a8fb8a51e409301dbd95697e
mpadge <mark.padgham@email.com>
2025-03-13 12:30:19 +0100

    make new 'mock_collate_org_data' test helper for #8

--- tests/testthat/helper-org-data.R
+++ tests/testthat/helper-org-data.R
@@ -28,3 +28,42 @@ generate_test_org_data <- function () {
 
     return (pkg_paths)
 }
+
+#' This uses the repometrics memoisations to enable 'collate_org_data' to be
+#' run on the org path without re-calculating.
+mock_collate_org_data <- function () {
+
+    dat <- mock_rm_data ()
+    path <- generate_test_pkg ()
+
+    # Then mock one set of org data:
+    metrics_all <- withr::with_options (
+        list ("repometrics_period" = 365.25 / 2),
+        metrics_over_end_dates (path, end_date = end_date, num_years = 1)
+    )
+    models_all <- withr::with_options (
+        list ("repometrics_period" = 365.25 / 2),
+        models_over_end_dates (path, end_date = end_date, num_years = 1)
+    )
+    dat <- list (
+        repo = repometrics::repometrics_data_repo (path, num_cores = 1L),
+        metrics = metrics_all,
+        models = models_all
+    )
+
+    org_dir <- fs::path (fs::path_temp (), "org")
+    if (!fs::dir_exists (org_dir)) {
+        fs::dir_create (org_dir)
+    }
+    path1 <- fs::dir_copy (path, fs::path (org_dir, "testpkg1"))
+    path2 <- fs::dir_copy (path, fs::path (org_dir, "testpkg2"))
+    fs::dir_delete (path)
+
+    d1 <- desc::desc_set ("Package" = "testpkg1", file = path1)
+    d2 <- desc::desc_set ("Package" = "testpkg2", file = path2)
+
+    saveRDS (dat, fs::path (fs::path_temp (), "testpkg1.Rds"))
+    saveRDS (dat, fs::path (fs::path_temp (), "testpkg2.Rds"))
+
+    return (org_dir)
+}
--- tests/testthat/test-models-across-orgs.R
+++ tests/testthat/test-models-across-orgs.R
@@ -8,37 +8,7 @@ get_end_date_seq <- utils::getFromNamespace ("get_end_date_seq", "repometrics")
 test_that ("collate across orgs", {
 
     Sys.setenv ("REPOMETRICS_TESTS" = "true")
-    dat <- mock_rm_data ()
-    path <- generate_test_pkg ()
-
-    # Then mock one set of org data:
-    metrics_all <- withr::with_options (
-        list ("repometrics_period" = 365.25 / 2),
-        metrics_over_end_dates (path, end_date = end_date, num_years = 1)
-    )
-    models_all <- withr::with_options (
-        list ("repometrics_period" = 365.25 / 2),
-        models_over_end_dates (path, end_date = end_date, num_years = 1)
-    )
-    dat <- list (
-        repo = repometrics::repometrics_data_repo (path, num_cores = 1L),
-        metrics = metrics_all,
-        models = models_all
-    )
-
-    org_dir <- fs::path (fs::path_temp (), "org")
-    if (!fs::dir_exists (org_dir)) {
-        fs::dir_create (org_dir)
-    }
-    path1 <- fs::dir_copy (path, fs::path (org_dir, "testpkg1"))
-    path2 <- fs::dir_copy (path, fs::path (org_dir, "testpkg2"))
-    fs::dir_delete (path)
-
-    d1 <- desc::desc_set ("Package" = "testpkg1", file = path1)
-    d2 <- desc::desc_set ("Package" = "testpkg2", file = path2)
-
-    saveRDS (dat, fs::path (fs::path_temp (), "testpkg1.Rds"))
-    saveRDS (dat, fs::path (fs::path_temp (), "testpkg2.Rds"))
+    org_dir <- mock_collate_org_data ()
 
     # Then the main call, which loads those pre-saved data rather than
     # re-generating:
@@ -48,7 +18,7 @@ test_that ("collate across orgs", {
         num_years = 1
     )
 
-    fs::dir_delete (c (path1, path2))
+    fs::dir_delete (org_dir)
 
     expect_type (org_data, "list")
     expect_named (org_data, c ("repos", "metrics", "models"))

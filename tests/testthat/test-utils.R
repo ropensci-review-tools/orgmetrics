1dac251f05956d2e7d77c91fb860e3640cd7c9a5
mpadge <mark.padgham@email.com>
2025-03-13 13:12:24 +0100

    fix testing list gh org repos

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,13 +1,14 @@
 test_that ("gh org repos", {
 
     Sys.setenv ("ORGMETRICS_TESTS" = "true")
+    Sys.setenv ("REPOMETRICS_TESTS" = "true") # for n_per_page --> 2
     org <- "ropensci"
     repo_names <- httptest2::with_mock_dir ("utils_gh_org", {
         list_gh_org_repos (org)
     })
 
     expect_type (repo_names, "character")
-    expect_length (repo_names, n_per_page_in_tests (30))
+    expect_length (repo_names, n_per_page_in_tests (100))
     expect_true (any (grepl ("\\/", repo_names))) # bare names only
 })
 

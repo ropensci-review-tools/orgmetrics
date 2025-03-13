1cd9d59acf14991bc4f4e1dbfb4b244679895fdc
mpadge <mark.padgham@email.com>
2025-03-13 10:51:40 +0100

    add metrics data to quarto dashboard for #8

--- R/quarto-dashboard-org.R
+++ R/quarto-dashboard-org.R
@@ -22,6 +22,8 @@ orgmetrics_dashboard <- function (data_org, fn_calls, emb_matrix, action = "prev
     data_models <- data_models_preprocess (data_org$models) |>
         dplyr::select (-org, -date) |>
         tidyr::pivot_longer (-package)
+    data_metrics <- data_metrics_to_df (data_org$metrics) |>
+        data_metrics_preproces ()
 
     requireNamespace ("jsonlite")
     requireNamespace ("quarto")
@@ -34,12 +36,78 @@ orgmetrics_dashboard <- function (data_org, fn_calls, emb_matrix, action = "prev
     path_dest <- fs::path (fs::path_temp (), "quarto")
     dir <- fs::dir_copy (path_src, path_dest, overwrite = TRUE)
     saveRDS (data_models, fs::path (dir, "results-models.Rds"))
+    saveRDS (data_metrics, fs::path (dir, "results-metrics.Rds"))
     saveRDS (fn_calls, fs::path (dir, "fn-calls.Rds"))
     saveRDS (emb_matrix, fs::path (dir, "emb-matrix.Rds"))
 
     withr::with_dir (dir, {
         do.call (eval (parse (text = quarto_action)), list ())
     })
+}
+
+#' Convert metrics data to rectangular `data.frame`.
+#'
+#' @param data_metrics An embedded list of [package] [date stamp] [metric],
+#' where some metrics are vectors of length > 1 which need to be reduced to
+#' single value.
+#' @return Single `data.frame` of metrics with aditional columns of [org, repo,
+#' date].
+#'
+#' @noRd
+data_metrics_to_df <- function (data_metrics) {
+
+    # Convert vectors to single values:
+    nms_counts <- c ("watchers", "issues", "prs") # committer_count
+    nms_msms <- c ("mean", "sd", "median", "sum") # pr_cmt_count, response_time
+    data_metrics <- lapply (data_metrics, function (i) {
+        lapply (i, function (j) {
+            lapply (j, function (k) {
+                if (identical (names (k), nms_counts)) {
+                    k <- k [["watchers"]] # committer_count
+                } else if (identical (names (k), nms_msms)) {
+                    k <- k [["mean"]] # pr_cmt_count, response_time
+                } else if (is.null (names (k)) && length (k) > 1L) {
+                    k <- mean (as.numeric (k))
+                } else if (is.null (names (k)) && length (k) == 0L) {
+                    k <- NA_real_
+                }
+                return (k)
+            })
+        })
+    })
+    # Then add pkg name and datestamp columns:
+    data_metrics_df <- lapply (seq_along (data_metrics), function (i) {
+        df <- lapply (seq_along (data_metrics [[i]]), function (j) {
+            data.frame (
+                org = gsub ("\\/.*$", "", names (data_metrics) [i]),
+                package = gsub ("^.*\\/", "", names (data_metrics) [i]),
+                date = names (data_metrics [[i]]) [j],
+                as.data.frame (data_metrics [[i]] [[j]])
+            )
+        })
+        do.call (rbind, df)
+    })
+    do.call (rbind, data_metrics_df) |>
+        dplyr::arrange (package, dplyr::desc (date))
+}
+
+#' Convert `data.frame` of metrics data returned from `data_metrics_to_df` to
+#' long-form tibble reduced to re-scaled values of latest metrics only for each
+#' package.
+#' @noRd
+data_metrics_preproces <- function (data_metrics) {
+
+    data_metrics |>
+        dplyr::mutate (
+            dplyr::across (dplyr::where (is.logical), as.numeric)
+        ) |>
+        dplyr::mutate (
+            dplyr::across (dplyr::where (is.numeric), ~ scale (.) [, 1])
+        ) |>
+        dplyr::group_by (package) |>
+        dplyr::slice_head (n = 1L) |>
+        dplyr::select (-org, -date) |>
+        tidyr::pivot_longer (-package)
 }
 
 #' Pre-process organization data by converting all model values to standard

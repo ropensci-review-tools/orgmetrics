80a5259c303a4d78b3f5e67f69c1e654133e4e3c
mpadge <mark.padgham@email.com>
2025-03-13 15:48:25 +0100

    update helper-tmp-pkg to never error

--- tests/testthat/helper-temp-pkg.R
+++ tests/testthat/helper-temp-pkg.R
@@ -3,8 +3,31 @@ generate_test_pkg <- function () {
     pkg <- system.file ("extdata", "testpkg.zip", package = "orgmetrics")
     pkg_dest <- fs::path (fs::path_temp (), "testpkg")
     if (fs::dir_exists (pkg_dest)) {
-        stop ("test package already exists", call. = FALSE)
+
+        # Check that pkg_dest holds same contents:
+        flist <- fs::dir_ls (pkg_dest, recurse = TRUE)
+        flist0 <- fs::path_rel (flist, fs::path_common (flist))
+
+        # new tmp version of same:
+        nm <- paste0 (sample (c (letters, LETTERS), size = 10), collapse = "")
+        pkg_dest_tmp <- fs::path (fs::path_temp (), nm)
+        fs::dir_create (pkg_dest_tmp)
+        flist1 <- unzip (pkg, exdir = pkg_dest_tmp)
+        pkg_dest_tmp_pkg <- fs::path (pkg_dest_tmp, basename (pkg_dest))
+
+        f1 <- fs::dir_ls (pkg_dest_tmp_pkg, recurse = TRUE)
+        flist1 <- fs::path_rel (f1, fs::path_common (f1))
+
+        is_same <- length (setdiff (flist0, flist1)) == 0L
+        if (is_same) {
+            flist <- fs::dir_ls (pkg_dest, recurse = TRUE)
+        } else {
+            flist <- fs::dir_ls (pkg_dest_tmp_pkg)
+        }
+
+    } else {
+        flist <- unzip (pkg, exdir = fs::path_temp ())
     }
-    flist <- unzip (pkg, exdir = fs::path_temp ())
+
     return (fs::path_common (flist))
 }

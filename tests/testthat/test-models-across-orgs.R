d5fb8edb8536b3453c5fa4a5df09f2b7d979651a
mpadge <mark.padgham@email.com>
2025-04-29 13:18:28 +0200

    update test collate_org_data with annual commits

--- tests/testthat/test-models-across-orgs.R
+++ tests/testthat/test-models-across-orgs.R
@@ -19,11 +19,13 @@ test_that ("collate_org_data output", {
     fs::dir_delete (org_dir)
 
     expect_type (org_data, "list")
-    expect_named (org_data, c ("repos", "metrics", "models"))
+    expect_named (org_data, c ("repos", "metrics", "models", "annual_commits"))
     npkgs <- 2L
     expect_length (org_data$repos, npkgs)
     expect_length (org_data$metrics, npkgs)
     expect_s3_class (org_data$models, "data.frame")
+    expect_s3_class (org_data$annual_commits, "data.frame")
+    expect_named (org_data$annual_commits, c ("year", "num_commits"))
     n_periods <- length (get_end_date_seq (end_date = end_date, num_years = 1))
     expect_equal (nrow (org_data$models), n_periods)
 })

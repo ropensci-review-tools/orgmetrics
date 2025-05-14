e5860639db4c3a15da18a93d4d70cc6a2caff7d3
mpadge <mark.padgham@email.com>
2025-05-14 12:33:36 +0200

    separate 'airtable_update_schema_names' fn for #19

--- R/airtable.R
+++ R/airtable.R
@@ -11,41 +11,12 @@ airtable_update_schema <- function (at_base_id = at_base_id) {
     fields <- schema$tables$fields [[table_num]]
 
     # ------ Update airtable field names -----
-    # airtable names can (at least initially) be either metadata "name""" or
-    # metadata "airtable_name".
-    index <- cbind (
-        match (fields$name, metrics_metadata$name),
-        match (fields$name, metrics_metadata$airtable_name)
+    updates_names <- airtable_update_schema_names (
+        at_base_id,
+        table_id,
+        fields,
+        metrics_metadata
     )
-    index <- apply (index, 1, function (i) {
-        ifelse (all (is.na (i)), NA_integer_, i [which (!is.na (i))] [1])
-    })
-    index <- cbind (seq_along (index), index)
-    index <- index [which (!is.na (index [, 2])), ]
-    # [, 1] -> fields; [, 2] -> mettrics_metadata
-
-    field_names_orig <- field_names_updated <- fields$name
-    field_names_updated [index [, 1]] <-
-        metrics_metadata$airtable_name [index [, 2]]
-
-    if (any (duplicated (field_names_updated))) {
-        dup <- field_names_updated [which (duplicated (field_names_updated))]
-        cli::cli_abort (
-            "Duplicated name of original field in airtable data: '{dup}'"
-        )
-    }
-
-    index <- which (field_names_updated != field_names_orig)
-    updates <- lapply (index, function (i) {
-        f_i <- match (field_names_orig [i], fields$name)
-        field_id <- fields$id [f_i]
-        airtabler::air_update_field (
-            base = at_base_id,
-            table_id = table_id,
-            field_id = field_id,
-            name = field_names_updated [i]
-        )
-    })
 
     # ------ Update airtable field descriptions -----
     # Re-fetch schema:
@@ -81,6 +52,48 @@ airtable_update_schema <- function (at_base_id = at_base_id) {
             table_id = table_id,
             field_id = fields_update$id [i],
             description = fields_update$description [i]
+        )
+    })
+}
+
+airtable_update_schema_names <- function (at_base_id,
+                                          table_id,
+                                          fields,
+                                          metrics_metadata) {
+
+    # airtable names can (at least initially) be either metadata "name""" or
+    # metadata "airtable_name".
+    index <- cbind (
+        match (fields$name, metrics_metadata$name),
+        match (fields$name, metrics_metadata$airtable_name)
+    )
+    index <- apply (index, 1, function (i) {
+        ifelse (all (is.na (i)), NA_integer_, i [which (!is.na (i))] [1])
+    })
+    index <- cbind (seq_along (index), index)
+    index <- index [which (!is.na (index [, 2])), ]
+    # [, 1] -> fields; [, 2] -> mettrics_metadata
+
+    field_names_orig <- field_names_updated <- fields$name
+    field_names_updated [index [, 1]] <-
+        metrics_metadata$airtable_name [index [, 2]]
+
+    if (any (duplicated (field_names_updated))) {
+        dup <- field_names_updated [which (duplicated (field_names_updated))]
+        cli::cli_abort (
+            "Duplicated name of original field in airtable data: '{dup}'"
+        )
+    }
+
+    index <- which (field_names_updated != field_names_orig)
+    updates <- lapply (index, function (i) {
+        f_i <- match (field_names_orig [i], fields$name)
+        field_id <- fields$id [f_i]
+        airtabler::air_update_field (
+            base = at_base_id,
+            table_id = table_id,
+            field_id = field_id,
+            name = field_names_updated [i]
         )
     })
 }

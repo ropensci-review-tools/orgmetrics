f5024521d214ae5e64ffafaaabbd9137ae7c80e9
mpadge <mark.padgham@email.com>
2025-06-04 14:12:46 +0200

    restructure dashboard fn for #31

--- R/quarto-dashboard-org.R
+++ R/quarto-dashboard-org.R
@@ -19,12 +19,77 @@ orgmetrics_dashboard <- function (data_org, fn_calls, emb_matrix, action = "prev
     # Suppress no visible binding notes:
     org <- package <- name <- login <- contributions <- result <- NULL
 
+    action <- match.arg (action, c ("preview", "render"))
+    quarto_action <- paste0 ("quarto::quarto_", action)
+
+    requireNamespace ("jsonlite")
+    requireNamespace ("quarto")
+    requireNamespace ("withr")
+
+    # -------- CHAOSS MODELS and METRICS: START -------
     data_models <- data_models_preprocess (data_org$models) |>
         dplyr::select (-org, -date) |>
         tidyr::pivot_longer (-package)
 
     data_metrics <- data_metrics_to_df (data_org$metrics)
+
     dates <- sort (unique (data_metrics$date), decreasing = TRUE)
+    repo_metrics <- dashboard_data_repo_metrics (data_metrics, dates)
+
+    data_metrics <- lapply (dates, function (d) {
+        data_metrics |>
+            dplyr::filter (date == d) |>
+            data_metrics_preprocess ()
+    })
+    names (data_metrics) <- dates
+    # -------- CHAOSS MODELS and METRICS: END -------
+
+    # -------- ADDITIONAL DATA: START -------
+    data_maintenance <- org_maintenance_metric (data_org)
+    data_abs <- ctb_absence (data_org)
+    data_resp <- issue_responses (data_org)
+    data_bugs <- issue_bugs (data_org)
+    data_contributors <- dashboard_data_contributors (data_org)
+    data_repo_src <- dashboard_data_repo_source (data_org)
+
+    data_pkgcheck <- lapply (data_org$repos, function (i) i$pkgcheck)
+    data_cran <- dashboard_data_cran (data_org)
+    data_gitlog <- dashboard_data_gitlog (data_org)
+    data_r_universe <- dashboard_data_r_universe (data_org)
+    # -------- ADDITIONAL DATA: END -------
+
+    path_src <- system.file ("extdata", "quarto", package = "orgmetrics")
+    path_dest <- fs::path (fs::path_temp (), "quarto")
+    dir <- fs::dir_copy (path_src, path_dest, overwrite = TRUE)
+
+    saveRDS (data_models, fs::path (dir, "results-models.Rds"))
+    saveRDS (data_metrics, fs::path (dir, "results-metrics.Rds"))
+    saveRDS (data_org$annual_commits, fs::path (dir, "results-annual-commits.Rds"))
+    saveRDS (data_org$annual_gh_activity, fs::path (dir, "results-annual-gh-activity.Rds"))
+    saveRDS (data_maintenance, fs::path (dir, "results-maintenance-org.Rds"))
+    saveRDS (data_contributors, fs::path (dir, "results-maintenance-contribs.Rds"))
+    saveRDS (data_repo_src, fs::path (dir, "results-data-repo-src.Rds"))
+    saveRDS (data_abs, fs::path (dir, "results-data-ctb-absence.Rds"))
+    saveRDS (data_resp, fs::path (dir, "results-data-issue-resp.Rds"))
+    saveRDS (data_bugs, fs::path (dir, "results-data-issue-bugs.Rds"))
+    saveRDS (data_pkgcheck, fs::path (dir, "results-pkgcheck.Rds"))
+    saveRDS (data_cran, fs::path (dir, "results-cran-checks.Rds"))
+    saveRDS (data_gitlog, fs::path (dir, "results-gitlog.Rds"))
+    saveRDS (repo_metrics, fs::path (dir, "results-repo-metrics.Rds"))
+    saveRDS (fn_calls, fs::path (dir, "fn-calls.Rds"))
+    saveRDS (emb_matrix, fs::path (dir, "emb-matrix.Rds"))
+
+    jsonlite::write_json (data_r_universe$data_is_on_r_univ, fs::path (dir, "data_is_on_r_univ.json"))
+    jsonlite::write_json (data_r_universe$r_univ_jobs, fs::path (dir, "data_r_univ_jobs.json"))
+    jsonlite::write_json (data_r_universe$r_univ_builds, fs::path (dir, "data_r_univ_builds.json"))
+
+    withr::with_dir (dir, {
+        do.call (eval (parse (text = quarto_action)), list ())
+    })
+}
+
+dashboard_data_repo_metrics <- function (data_metrics, dates) {
+
     # Repo summaries for repo page:
     repo_summary_vars <- c (
         "change_req_n_opened",
@@ -56,17 +121,10 @@ orgmetrics_dashboard <- function (data_org, fn_calls, emb_matrix, action = "prev
     repo_metrics <- split (repo_metrics, f = as.factor (repo_metrics$package)) |>
         lapply (function (m) dplyr::select (m, -package))
 
-    data_metrics <- lapply (dates, function (d) {
-        data_metrics |>
-            dplyr::filter (date == d) |>
-            data_metrics_preprocess ()
-    })
-    names (data_metrics) <- dates
+    return (repo_metrics)
+}
 
-    data_maintenance <- org_maintenance_metric (data_org)
-    data_abs <- ctb_absence (data_org)
-    data_resp <- issue_responses (data_org)
-    data_bugs <- issue_bugs (data_org)
+dashboard_data_contributors <- function (data_org) {
 
     data_contributors <- lapply (data_org$repos, function (repo) {
         ctbs_gh <- repo$rm$contribs_from_gh_api |>
@@ -78,15 +136,21 @@ orgmetrics_dashboard <- function (data_org, fn_calls, emb_matrix, action = "prev
     })
     names (data_contributors) <- gsub ("^.*\\/", "", names (data_contributors))
 
-    # Plus mapping from repos to org/repo:
+    return (data_contributors)
+}
+
+# Map from repos to org/repo:
+dashboard_data_repo_source <- function (data_org) {
+
     data_repo_src <- names (data_org$repos)
-    data_repo_src <- data.frame (
+    data.frame (
         package = gsub ("^.*\\/", "", data_repo_src),
         orgrepo = data_repo_src,
         url = paste0 ("https://github.com/", data_repo_src)
     )
+}
 
-    data_pkgcheck <- lapply (data_org$repos, function (i) i$pkgcheck)
+dashboard_data_cran <- function (data_org) {
 
     data_cran <- lapply (data_org$repos, function (i) {
         if (!inherits (i$cran_checks, "data.frame")) {
@@ -104,20 +168,28 @@ orgmetrics_dashboard <- function (data_org, fn_calls, emb_matrix, action = "prev
     attr (data_cran, "not_cran") <- names (not_cran)
     rownames (data_cran) <- NULL
 
+    return (data_cran)
+}
+
+dashboard_data_gitlog <- function (data_org) {
+
     data_gitlog <- t (vapply (data_org$repos, function (i) {
         c (
             num_commits = as.character (nrow (i$rm$gitlog)),
             first_commit = as.character (as.Date (min (i$rm$gitlog$timestamp)))
         )
     }, character (2L)))
-    data_gitlog <- data.frame (
+
+    data.frame (
         package = gsub ("^.*\\/", "", rownames (data_gitlog)),
         num_commits = as.integer (data_gitlog [, 1]),
         first_commit = as.Date (data_gitlog [, 2]),
         row.names = NULL
     )
+}
 
-    # -------- R-UNIVERSE START -------
+dashboard_data_r_universe <- function (data_org) {
+
     dat <- lapply (data_org$repos, function (i) {
         c (i$rm$r_universe$universe, i$rm$r_universe$package)
     })
@@ -145,40 +217,10 @@ orgmetrics_dashboard <- function (data_org, fn_calls, emb_matrix, action = "prev
     r_univ_builds <- do.call (rbind, r_univ_builds)
     rownames (r_univ_builds) <- NULL
     r_univ_builds <- split (r_univ_builds, f = as.factor (r_univ_builds$package))
-    # -------- R-UNIVERSE END -------
 
-    requireNamespace ("jsonlite")
-    requireNamespace ("quarto")
-    requireNamespace ("withr")
-
-    action <- match.arg (action, c ("preview", "render"))
-    quarto_action <- paste0 ("quarto::quarto_", action)
-
-    path_src <- system.file ("extdata", "quarto", package = "orgmetrics")
-    path_dest <- fs::path (fs::path_temp (), "quarto")
-    dir <- fs::dir_copy (path_src, path_dest, overwrite = TRUE)
-    saveRDS (data_models, fs::path (dir, "results-models.Rds"))
-    saveRDS (data_metrics, fs::path (dir, "results-metrics.Rds"))
-    saveRDS (data_org$annual_commits, fs::path (dir, "results-annual-commits.Rds"))
-    saveRDS (data_org$annual_gh_activity, fs::path (dir, "results-annual-gh-activity.Rds"))
-    saveRDS (data_maintenance, fs::path (dir, "results-maintenance-org.Rds"))
-    saveRDS (data_contributors, fs::path (dir, "results-maintenance-contribs.Rds"))
-    saveRDS (data_repo_src, fs::path (dir, "results-data-repo-src.Rds"))
-    saveRDS (data_abs, fs::path (dir, "results-data-ctb-absence.Rds"))
-    saveRDS (data_resp, fs::path (dir, "results-data-issue-resp.Rds"))
-    saveRDS (data_bugs, fs::path (dir, "results-data-issue-bugs.Rds"))
-    saveRDS (data_pkgcheck, fs::path (dir, "results-pkgcheck.Rds"))
-    saveRDS (data_cran, fs::path (dir, "results-cran-checks.Rds"))
-    saveRDS (data_gitlog, fs::path (dir, "results-gitlog.Rds"))
-    saveRDS (repo_metrics, fs::path (dir, "results-repo-metrics.Rds"))
-    saveRDS (fn_calls, fs::path (dir, "fn-calls.Rds"))
-    saveRDS (emb_matrix, fs::path (dir, "emb-matrix.Rds"))
-
-    jsonlite::write_json (data_is_on_r_univ, fs::path (dir, "data_is_on_r_univ.json"))
-    jsonlite::write_json (r_univ_jobs, fs::path (dir, "data_r_univ_jobs.json"))
-    jsonlite::write_json (r_univ_builds, fs::path (dir, "data_r_univ_builds.json"))
-
-    withr::with_dir (dir, {
-        do.call (eval (parse (text = quarto_action)), list ())
-    })
+    list (
+        data_is_on_r_univ = data_is_on_r_univ,
+        r_univ_jobs = r_univ_jobs,
+        r_univ_builds = r_univ_builds
+    )
 }

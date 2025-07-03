9599f2b103e942f2c76c4883210ef61e6932c80f
mpadge <mark.padgham@email.com>
2025-07-03 14:00:11 +0200

    update collate_org_data for #19 to accept 'packages.json' as main input

--- R/data-cm-orgs.R
+++ R/data-cm-orgs.R
@@ -5,40 +5,37 @@ metrics_over_end_dates <-
 models_over_end_dates <-
     utils::getFromNamespace ("models_over_end_dates", "repometrics")
 
-orgmetrics_collate_org_data <- function (org_paths, end_date = Sys.Date (), num_years = 3) {
+orgmetrics_collate_org_data <- function (pkgs_json, end_date = Sys.Date (), num_years = 3) {
 
-    pkgs <- fs::dir_ls (org_paths, type = "directory")
-    org_repo <- t (vapply (pkgs, function (p) {
-        p_s <- fs::path_split (p) [[1]]
-        n <- length (p_s)
-        p_s [(n - 1):n]
-    }, character (2L), USE.NAMES = FALSE))
+    requireNamespace ("jsonlite", quietly = TRUE)
+
+    pkgs_dat <- jsonlite::read_json (pkgs_json, simplify = TRUE)
+    pkgs_dat$path <- fs::path (fs::path_dir (pkgs_json), pkgs_dat$path)
     is_test_env <- Sys.getenv ("ORGMETRICS_TESTS") == "true"
 
-    data <- lapply (seq_along (pkgs), function (i) {
+    # pkgs_dat has [path, orgrepo, root, is_r_pkg]
+    data <- lapply (seq_len (nrow (pkgs_dat)), function (i) {
 
-        is_r <- pkgs_are_r (paste0 (org_repo [i, ], collapse = "/"))
-        if (!is_r && !is_test_env) {
+        if (!pkgs_dat$is_r_pkg [i] && !is_test_env) {
             return (NULL)
         }
 
-        cli::cli_alert_info ("[{i} / {length(pkgs)}]: {pkgs[i]}")
-        path_i <- pkgs [i]
-        pkg_i <- basename (path_i)
+        cli::cli_alert_info ("[{i} / {nrow (pkgs_dat)}]: {pkgs_dat$orgrepo[i]}")
+        pkg_i <- basename (pkgs_dat$orgrepo [i])
         f_tmp <- fs::path (fs::path_temp (), paste0 (pkg_i, ".Rds"))
         if (fs::file_exists (f_tmp)) {
             dat_i <- readRDS (f_tmp)
         } else {
-            dat_repo <- repometrics::repometrics_data_repo (path_i)
+            dat_repo <- repometrics::repometrics_data_repo (pkgs_dat$path [i])
             dat_i <- list (
                 repo = dat_repo_to_end_date (dat_repo, end_date = end_date),
                 metrics = metrics_over_end_dates (
-                    path_i,
+                    pkgs_dat$path [i],
                     end_date = end_date,
                     num_years = num_years
                 ),
                 models = models_over_end_dates (
-                    path_i,
+                    pkgs_dat$path [i],
                     end_date = end_date,
                     num_years = num_years
                 )

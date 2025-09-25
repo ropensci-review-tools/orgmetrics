e81d90271f5fcde0c08eecc1ae417fc62baaa039
mpadge <mark.padgham@email.com>
2025-09-25 10:13:48 +0200

    start 'add_org_contributor_data' fn for #62

--- R/data-contributors.R
+++ R/data-contributors.R
@@ -1,6 +1,37 @@
 gh_user_general <- utils::getFromNamespace ("gh_user_general", "repometrics")
 gh_user_follow <- utils::getFromNamespace ("gh_user_follow", "repometrics")
 
+#' Add full data on each contributor after assembling all repo-level data.
+#'
+#' @param data_org Partial result of 'orgmetrics_collate_org_data', including
+#' full data on all repos.
+#' @return Same data with additional 'contributors' item.
+add_org_contributor_data <- function (data_org) {
+
+    ctbs <- get_unique_ctbs (data_org)
+
+}
+
+get_unique_ctbs <- function (data_org) {
+
+    ctbs <- lapply (data_org$repos, function (repo) {
+        repo$rm$contribs_from_gh_api$login
+    }) |>
+        unlist () |>
+        unname () |>
+        unique ()
+    ctbs <- ctbs [which (!grepl ("\\[bot\\]", ctbs))]
+    ctbs <- setdiff (ctbs, "ci-bot")
+    # Running 'url_exists' takes way too long, but all ctbs exist as of Sep
+    # 2025, so no checks done.
+
+    # "Copilot" is a contributor, but doesn't exist either as a URL or an API
+    # endpoint:
+    ctbs <- setdiff (ctbs, "Copilot")
+
+    return (ctbs)
+}
+
 # These are modified versions from repometrics functions. The GraphQL API only
 # allows "from" and "to" dates which span 1 year or less. The "gh_user_commits"
 # function here calls the query multiple times, adjusting the "end_year" each
